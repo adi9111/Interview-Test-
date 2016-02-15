bceb6b1c2c85de4e0065f091864a76379b2b3b33
Adrian Filipczuk <adrianfilipczuk@gmail.com>
2016-02-15 20:20:59 +0100

    Interview Test

---
+++ InterviewTest/src/View.java
@@ -0,0 +1,30 @@
+
+public class View {
+	
+	public void display()
+	{
+		
+		System.out.println("Text a massage ");
+		int x=0;
+		while(x<10)
+		{
+			
+			System.out.println(x);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			x++; 
+			
+			if(x>=10)
+			{
+				System.out.println("where running out ");
+			}
+		}
+	}
+
+	
+
+}
